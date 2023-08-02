b4945ced9053a48b5d6cba50da1ae000286d3935
Alexandre Gusmao <alex_vng@hotmail.com>
2023-08-02 18:53:21 +0100

    Successfully added a + button on tabs, that generate new tabs. For now, all these tabs have equal properties. It will be important to define them in their respective class after the user decides which type of tab to open. I also established that the first tab will be a special tab, which will contain the linear regression from all calibration trials, and the thickness of all the material tabs, probably with the option to average them.

--- AEL_beta2.py
+++ AEL_beta2.py
@@ -21,6 +21,12 @@ import ctypes
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
+######################################################
+
+def handleTabChange(event):
+
+    Tabs.tab_add(Notebook.value)
+
 class Skeleton:
 
     def __init__(self):
@@ -31,12 +37,6 @@ class Skeleton:
         self.main.title('ARC_TF')
         self.main.state('zoomed')
         self.main.configure(background = 'dark grey')
-
-        ####### A variavel do Notebook ##########
-
-        self.notebook = ttk.Notebook(self.main)   
-        self.notebook.pack(expand = True, fill = 'both')    
-        self.notebook.enable_traversal()
 
         ############## A barra de Ferramentas e opcoes #################
     
@@ -67,34 +67,85 @@ class Skeleton:
         algorithm_menu.add_radiobutton(label = "Manual Selection")
         algorithm_menu.add_radiobutton(label = "Threshold Input")
 
+    def run(self):
+        self.main.mainloop()
 
+class Tabs():
+
+
+    def First_Tabs(self):
+
+        ####### A variavel do Notebook ##########
+
+        self.value = 0
+
+        self.notebook = ttk.Notebook(window.main)   
+        self.notebook.pack(expand = True, fill = 'both')    
+        self.notebook.enable_traversal()
+        
         """ Cada tab do Notebook vai ter uma frame que ocupa a janela na sua
             totalidade. Assim temos a CTFrame para os dados de calibracao;
             a MTFrame para os dados de ensaios das peliculas, e a
             CRFrame que aglomera todos os resulados finais obtidos. """
         
-        self.CTFrame = tk.Frame(self.notebook, bg = 'blue')
-        self.MTFrame = tk.Frame(self.notebook, bg = 'red')
-        self.CRFrame = tk.Frame(self.notebook, bg = 'green')
+        """ self.CTFrame = tk.Frame(self.notebook, bg = 'blue')
+        self.MTFrame = tk.Frame(self.notebook, bg = 'red') """
+        self.CRFrame = tk.Frame(self.notebook, bg = 'dark grey')
+        self.PlusFrame = tk.Frame(self.notebook)
         
         
-        self.notebook.add(self.CTFrame, text = 'Calibration Trials')
-        self.notebook.add(self.MTFrame, text = 'Material Trials')
-        self.notebook.add(self.CRFrame, text = 'Calculated Results')
+        """ self.notebook.add(self.CTFrame, text = 'Calibration Trials')
+        self.notebook.add(self.MTFrame, text = 'Material Trials') """
+        self.notebook.add(self.CRFrame, text = 'Final Results')
+        self.notebook.add(self.PlusFrame, text = '+')
 
+    @staticmethod
+    def tab_add(num):
+        if Notebook.notebook.select() == Notebook.notebook.tabs()[-1] and len(Notebook.notebook.tabs()) < 17:
+            index = len(Notebook.notebook.tabs()) - 1
+            Tab[num] = tk.Frame(Notebook.notebook)
+            print(Tab[num])
+            Notebook.notebook.insert(index, Tab[num], text = "teste")
+            Notebook.notebook.select(index)    
+            Notebook.value = Notebook.value + 1
 
-    def run(self):
-        self.main.mainloop()
 
 
 window = Skeleton()
+Notebook = Tabs()
+Notebook.First_Tabs()
 
-GraphicFrame_Calib = tk.Frame(window.CTFrame, borderwidth = 5, relief = 'ridge')
+############## Tabs variaveis para serem criadas ###############
+
+tab1 = Tabs()
+tab2 = Tabs()
+tab3 = Tabs()
+tab4 = Tabs()
+tab5 = Tabs()
+tab6 = Tabs()
+tab7 = Tabs()
+tab8 = Tabs()
+tab9 = Tabs()
+tab10 = Tabs()
+tab11 = Tabs()
+tab12 = Tabs()
+tab13 = Tabs()
+tab14 = Tabs()
+tab15 = Tabs()
+
+Tab = [tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12,
+       tab13, tab14, tab15]
+
+Notebook.notebook.bind("<<NotebookTabChanged>>", handleTabChange)
+
+##############################################################
+
+""" GraphicFrame_Calib = tk.Frame(Notebook.CTFrame, borderwidth = 5, relief = 'ridge')
 GraphicFrame_Calib.grid(column = 0, row = 0, sticky = "nw", pady = 5)
 tk.Label(GraphicFrame_Calib, text = 'Calibration').grid(row = 0)
 
-GraphicFrame_Material = tk.Frame(window.MTFrame, borderwidth = 5, relief = 'ridge')
+GraphicFrame_Material = tk.Frame(Notebook.MTFrame, borderwidth = 5, relief = 'ridge')
 GraphicFrame_Material.grid(column = 0, row = 0, sticky = "nw", pady = 5)
-tk.Label(GraphicFrame_Material, text = 'Material').grid(row = 0)
+tk.Label(GraphicFrame_Material, text = 'Material').grid(row = 0) """
 
 window.run()
