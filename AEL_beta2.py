dba38d8895803d76ddacbc1b7520d1f1a3f7a809
Alexandre Gusmao <alex_vng@hotmail.com>
2023-08-03 17:11:07 +0100

    Was able to implement a popup window that would open the different tabs available. Will alocate the Tabs options to 1 new class, with different methods, to configure the work environment for the two types of analysis (calibration and material) the software will perform.

--- AEL_beta2.py
+++ AEL_beta2.py
@@ -25,7 +25,22 @@ ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 def handleTabChange(event):
 
-    Tabs.tab_add(Notebook.value)
+    if Notebook.notebook.select() == Notebook.notebook.tabs()[-1] and len(Notebook.notebook.tabs()) < 15:
+            
+        wng.popup('Tab Selector Menu')
+
+        tk.Label(wng.warning, text = 'Please Select New Type of Tab to Open \n').pack()
+        tk.Button(wng.warning, command = lambda: Tabs.tab_change(1), 
+                        text = 'Calibration Trial').pack()
+        tk.Button(wng.warning, command = lambda: Tabs.tab_change(2), 
+                        text = 'Material Trial').pack()
+        tk.Label(wng.warning, text ='\n').pack()
+        tk.Button(wng.warning, text = 'Return',
+                    command = lambda : Tabs.tab_change(3)).pack()
+
+    elif len(Notebook.notebook.tabs()) >= 15:
+        Notebook.notebook.forget(14)
+
 
 class Skeleton:
 
@@ -44,76 +59,113 @@ class Skeleton:
         self.main.config(menu = menu)
 
             # O tipico File Menu. Hao de haver mais opcoes no futuro
-        file_menu = tk.Menu(menu, tearoff = False) 
-        menu.add_cascade(label = 'File', menu = file_menu)
-        file_menu.add_command(label = 'Upload Data')
-        file_menu.add_command(label = 'Run')
-        file_menu.add_separator()
-        file_menu.add_command(label = "Save Data")
-        file_menu.add_command(label = "Delete Trial")
-        file_menu.add_command(label = "Manage Alpha Particles Source Values")
-        file_menu.add_separator()
-        file_menu.add_command(label = "Help")
+        __file_menu = tk.Menu(menu, tearoff = False) 
+        menu.add_cascade(label = 'File', menu = __file_menu)
+        __file_menu.add_command(label = 'Upload Plotting Data')
+        __file_menu.add_command(label = 'Remove Current Plotting Data')
+        __file_menu.add_command(label = 'Remove Algorithm Results')
+        __file_menu.add_separator()
+        __file_menu.add_command(label = "Save Results")
+        __file_menu.add_command(label = "Delete Trial")
+        __file_menu.add_command(label = "Manage Alpha Particles Source Values")
+        __file_menu.add_separator()
+        __file_menu.add_command(label = 'Exit Program', command = self.main.quit)
 
             # Indica ao programa qual a fonte de particulas alfa
-        source_menu = tk.Menu(menu, tearoff = False)
-        menu.add_cascade(label = 'Alpha Particles Source', menu = source_menu)
-        source_menu.add_radiobutton(label = "Ra 226")
-        source_menu.add_radiobutton(label = "Ur 232")
+        __source_menu = tk.Menu(menu, tearoff = False)
+        menu.add_cascade(label = 'Alpha Particles Source', menu = __source_menu)
+        __source_menu.add_radiobutton(label = "Ra 226")
+        __source_menu.add_radiobutton(label = "Ur 232")
 
             # Indica ao programa qual o algoritmo a correr
-        algorithm_menu = tk.Menu(menu, tearoff = False)
-        menu.add_cascade(label = 'Algorithm', menu = algorithm_menu)
-        algorithm_menu.add_radiobutton(label = "Manual Selection")
-        algorithm_menu.add_radiobutton(label = "Threshold Input")
+        __algorithm_menu = tk.Menu(menu, tearoff = False)
+        menu.add_cascade(label = 'Algorithm', menu = __algorithm_menu)
+        __algorithm_menu.add_radiobutton(label = "Manual Selection")
+        __algorithm_menu.add_radiobutton(label = "Threshold Input")
+
+            # Gere as tabs do programa
+        __tabs_menu = tk.Menu(menu, tearoff = False)
+        menu.add_cascade(label = 'Manage Tabs', menu = __tabs_menu)
+        __tabs_menu.add_command(label = 'Add Calibration Tab')
+        __tabs_menu.add_command(label = 'Add Material Tab')
+        __tabs_menu.add_separator()
+        __tabs_menu.add_command(label = 'Remove Current Tab')
+
+            # Corre o algoritmo
+        menu.add_command(label = 'Run')
+
+            # Abre o ficheiro Help
+        menu.add_command(label = "Help")
 
     def run(self):
+        #### O metodo que permite o programa manter-se aberto
         self.main.mainloop()
 
-class Tabs():
+class Warnings():
+    def popup(self, name):
 
+        self.warning = tk.Toplevel(window.main)
+        self.warning.title(name)
+        self.warning.geometry('700x300')
+        self.warning.grab_set()
+
+class Tabs():
 
     def First_Tabs(self):
 
         ####### A variavel do Notebook ##########
-
-        self.value = 0
-
         self.notebook = ttk.Notebook(window.main)   
-        self.notebook.pack(expand = True, fill = 'both')    
-        self.notebook.enable_traversal()
+        self.notebook.pack(expand = True, fill = 'both') # Expande a frame da tab ate ao final
+                                                         # da janela
+        self.notebook.enable_traversal() # Permite o uso de Ctrl+Tab para circular entre tabs
         
-        """ Cada tab do Notebook vai ter uma frame que ocupa a janela na sua
-            totalidade. Assim temos a CTFrame para os dados de calibracao;
-            a MTFrame para os dados de ensaios das peliculas, e a
-            CRFrame que aglomera todos os resulados finais obtidos. """
-        
-        """ self.CTFrame = tk.Frame(self.notebook, bg = 'blue')
-        self.MTFrame = tk.Frame(self.notebook, bg = 'red') """
+        ########### As frames principais - Os resultados e a frame de adicionar
         self.CRFrame = tk.Frame(self.notebook, bg = 'dark grey')
         self.PlusFrame = tk.Frame(self.notebook)
         
-        
-        """ self.notebook.add(self.CTFrame, text = 'Calibration Trials')
-        self.notebook.add(self.MTFrame, text = 'Material Trials') """
+        ############# Aqui adicionam-se as frames iniciadas acima
         self.notebook.add(self.CRFrame, text = 'Final Results')
         self.notebook.add(self.PlusFrame, text = '+')
 
-    @staticmethod
-    def tab_add(num):
-        if Notebook.notebook.select() == Notebook.notebook.tabs()[-1] and len(Notebook.notebook.tabs()) < 17:
-            index = len(Notebook.notebook.tabs()) - 1
-            Tab[num] = tk.Frame(Notebook.notebook)
-            print(Tab[num])
-            Notebook.notebook.insert(index, Tab[num], text = "teste")
-            Notebook.notebook.select(index)    
-            Notebook.value = Notebook.value + 1
+        ########### Variavel para contar o numero de separadores 
+        self.value = 0
 
+        ############ Este comando adiciona o evento a funcao, para adicionar frames
+        self.notebook.bind("<<NotebookTabChanged>>", handleTabChange)      
+
+
+    @staticmethod
+    def tab_change(num):
+
+        index = len(Notebook.notebook.tabs()) - 1
+        Tab[Notebook.value] = tk.Frame(Notebook.notebook)
+
+        if num == 3:
+
+            Notebook.notebook.select(index - 1)
+            wng.warning.destroy()
+            
+
+        elif num == 1:
+
+            Notebook.notebook.insert(index, Tab[Notebook.value], text = "Calibration Trial")
+            Notebook.notebook.select(index)
+            Notebook.value = Notebook.value + 1
+            wng.warning.destroy()
+
+            
+        elif num == 2:
+
+            Notebook.notebook.insert(index, Tab[Notebook.value], text = "Material Trial")
+            Notebook.notebook.select(index)
+            Notebook.value = Notebook.value + 1
+            wng.warning.destroy()
 
 
 window = Skeleton()
 Notebook = Tabs()
 Notebook.First_Tabs()
+wng = Warnings()
 
 ############## Tabs variaveis para serem criadas ###############
 
@@ -130,13 +182,9 @@ tab10 = Tabs()
 tab11 = Tabs()
 tab12 = Tabs()
 tab13 = Tabs()
-tab14 = Tabs()
-tab15 = Tabs()
 
-Tab = [tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12,
-       tab13, tab14, tab15]
-
-Notebook.notebook.bind("<<NotebookTabChanged>>", handleTabChange)
+Tab = [tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, 
+       tab9, tab10, tab11, tab12, tab13]
 
 ##############################################################
 
