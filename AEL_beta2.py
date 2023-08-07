3c79d3659a815cf8e744fd2f3fa9a0b3c26bb125
Alexandre Gusmao <alex_vng@hotmail.com>
2023-08-07 12:15:06 +0100

    Was able to incorporate the notebook and its frames successfully. Added a modified plotting function which fixes some of alpha1 version's problems. Will join the frames and tab class variables, plus a string-name for the data files to be written, in an array, so every function will act on the tab the user is currently at.

--- AEL_beta2.py
+++ AEL_beta2.py
@@ -23,6 +23,72 @@ ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 ######################################################
 
+#############################################
+# Esta funcao recebe os dados dos ficheiros externos
+# e insere os graficos na frame grande do GUI.
+# Ao mesmo tempo cria um txt para outras funções
+# acederem aos dados
+#############################################
+def Plot(File, Name):
+
+    Channel = []    #Lista vazia para guardar o Channel
+    Counts = []     #Lista vazia para guardar os Counts
+
+    num = Notebook.notebook.select()  # Devolve o id da tab onde o utilizador se encontra
+    num = num[-1]                     # Vai buscar o numero na string do id
+    num = int(num) - 3                # Altera-se o valor para corresponder aos indices da lista
+
+    for widgets in TabType[num].GraphicFrame.winfo_children():
+        widgets.destroy()         # Este 'for' destroi grafcos antigos e constoi novos
+
+    Data = open("Data.txt", "w")
+
+    if Name[-4:] == ".mca":         #Por enquanto esta configurado para os ficheiros 
+                                    # da maquina para AEL. Se for configurado RBS
+                                    #ha-de-se incluir outro if.
+        for i in range(12, len(File) - 1):  
+            Counts.append(float(File[i]))
+            Channel.append(i-11)
+            Data.write(str(Counts[i-12])+"\n")
+
+        # Ciclo for para adquirir os valores dos dados
+
+    Data.close()
+
+    figure = Figure(figsize = (6,4), dpi = 100) #A figura contem o grafico
+    figure_canvas = FigureCanvasTkAgg(figure, TabType[num].GraphicFrame) #A class FigureCanvasTkAgg
+    #liga o matplotlib ao tkinter
+
+    NavigationToolbar2Tk(figure_canvas, TabType[num].GraphicFrame) # Esta linha permite que as ferramentas
+    #do matplotlip aparecam na interface do tkinter
+
+    #Aqui inicia-se o grafico com os dados e os eixos
+    axes = figure.add_subplot() #
+    axes.plot(Channel, Counts, 'D')
+    axes.set_title("Calibration Run")
+    axes.set_xlabel('Channel')
+    axes.set_ylabel('Counts')
+
+    #Por fim, acrescenta-se a geometria do tkinter
+    figure_canvas.get_tk_widget().pack()
+
+
+  
+##############################################################################
+#Esta funcao ira ler o ficheiro input.
+##############################################################################
+def FileReader(): 
+
+    domain = (('text files', '*.mca'), ('all files', '*.*'))
+    filename = fd.askopenfilename(title = 'Open a file', initialdir = '.', filetypes = domain)
+    ###########INSERIR JANELA QUE NAO PERMITE FICHEIROS QUE NAO SEJAM MCA 
+    OpenFile = open(filename, 'r')
+    file = OpenFile.read()
+    OpenFile.close()
+    file = file.splitlines()
+    Plot(file, filename)
+
+
 def handleTabChange(event):
 
     if Notebook.notebook.select() == Notebook.notebook.tabs()[-1] and len(Notebook.notebook.tabs()) < 15:
@@ -39,8 +105,7 @@ def handleTabChange(event):
                     command = lambda : Tabs.tab_change(3)).pack()
 
     elif len(Notebook.notebook.tabs()) >= 15:
-        Notebook.notebook.forget(14)
-
+        Notebook.notebook.hide(14)
 
 class Skeleton:
 
@@ -61,7 +126,7 @@ class Skeleton:
             # O tipico File Menu. Hao de haver mais opcoes no futuro
         __file_menu = tk.Menu(menu, tearoff = False) 
         menu.add_cascade(label = 'File', menu = __file_menu)
-        __file_menu.add_command(label = 'Upload Plotting Data')
+        __file_menu.add_command(label = 'Upload Plotting Data', command = FileReader)
         __file_menu.add_command(label = 'Remove Current Plotting Data')
         __file_menu.add_command(label = 'Remove Algorithm Results')
         __file_menu.add_separator()
@@ -86,8 +151,8 @@ class Skeleton:
             # Gere as tabs do programa
         __tabs_menu = tk.Menu(menu, tearoff = False)
         menu.add_cascade(label = 'Manage Tabs', menu = __tabs_menu)
-        __tabs_menu.add_command(label = 'Add Calibration Tab')
-        __tabs_menu.add_command(label = 'Add Material Tab')
+        __tabs_menu.add_command(label = 'Add Calibration Tab', command = lambda: Tabs.tab_change(1))
+        __tabs_menu.add_command(label = 'Add Material Tab', command = lambda: Tabs.tab_change(2))
         __tabs_menu.add_separator()
         __tabs_menu.add_command(label = 'Remove Current Tab')
 
@@ -101,7 +166,7 @@ class Skeleton:
         #### O metodo que permite o programa manter-se aberto
         self.main.mainloop()
 
-class Warnings():
+class Warnings:
     def popup(self, name):
 
         self.warning = tk.Toplevel(window.main)
@@ -109,7 +174,7 @@ class Warnings():
         self.warning.geometry('700x300')
         self.warning.grab_set()
 
-class Tabs():
+class Tabs:
 
     def First_Tabs(self):
 
@@ -133,12 +198,34 @@ class Tabs():
         ############ Este comando adiciona o evento a funcao, para adicionar frames
         self.notebook.bind("<<NotebookTabChanged>>", handleTabChange)      
 
+    def CalibTab(self):
+        TabFrame[Notebook.value].columnconfigure(0, weight= 3)
+        TabFrame[Notebook.value].columnconfigure(1, weight = 1)
+        TabFrame[Notebook.value].columnconfigure(2, weight = 2)
+        TabFrame[Notebook.value].rowconfigure(0, weight = 4)
+        TabFrame[Notebook.value].rowconfigure(1, weight = 1)
+        self.GraphicFrame = tk.Frame(TabFrame[Notebook.value], borderwidth = 5, relief = 'ridge')
+        self.GraphicFrame.grid(column = 0, row = 0, sticky = "nw", pady = 5, columnspan = 2)
+
+        self.DataFrame = tk.Frame(TabFrame[Notebook.value], borderwidth = 5, relief = 'ridge')
+        self.DataFrame.grid(column = 2, row = 0, sticky = "ne", pady = 5)
+        tk.Label(self.DataFrame, text = 'Hallo').grid()
+        
+        
+    def MatTab(self):
+        TabFrame[Notebook.value].columnconfigure(0, weight= 3)
+        TabFrame[Notebook.value].columnconfigure(1, weight = 1)
+        TabFrame[Notebook.value].columnconfigure(2, weight = 2)
+        TabFrame[Notebook.value].rowconfigure(0, weight = 4)
+        TabFrame[Notebook.value].rowconfigure(1, weight = 1)
+        self.GraphicFrame = tk.Frame(TabFrame[Notebook.value], borderwidth = 5, relief = 'ridge')
+        self.GraphicFrame.grid(column = 0, row = 0, sticky = "nw", pady = 5, columnspan = 2)
+        
 
     @staticmethod
     def tab_change(num):
 
         index = len(Notebook.notebook.tabs()) - 1
-        Tab[Notebook.value] = tk.Frame(Notebook.notebook)
 
         if num == 3:
 
@@ -147,8 +234,9 @@ class Tabs():
             
 
         elif num == 1:
-
-            Notebook.notebook.insert(index, Tab[Notebook.value], text = "Calibration Trial")
+            
+            TabType[Notebook.value].CalibTab()
+            Notebook.notebook.insert(index, TabFrame[Notebook.value], text = "Calibration Trial")
             Notebook.notebook.select(index)
             Notebook.value = Notebook.value + 1
             wng.warning.destroy()
@@ -156,7 +244,8 @@ class Tabs():
             
         elif num == 2:
 
-            Notebook.notebook.insert(index, Tab[Notebook.value], text = "Material Trial")
+            TabType[Notebook.value].MatTab()
+            Notebook.notebook.insert(index, TabFrame[Notebook.value], text = "Material Trial")
             Notebook.notebook.select(index)
             Notebook.value = Notebook.value + 1
             wng.warning.destroy()
@@ -169,31 +258,40 @@ wng = Warnings()
 
 ############## Tabs variaveis para serem criadas ###############
 
-tab1 = Tabs()
-tab2 = Tabs()
-tab3 = Tabs()
-tab4 = Tabs()
-tab5 = Tabs()
-tab6 = Tabs()
-tab7 = Tabs()
-tab8 = Tabs()
-tab9 = Tabs()
-tab10 = Tabs()
-tab11 = Tabs()
-tab12 = Tabs()
-tab13 = Tabs()
+tab1 = tk.Frame(Notebook.notebook)
+tab2 = tk.Frame(Notebook.notebook)
+tab3 = tk.Frame(Notebook.notebook)
+tab4 = tk.Frame(Notebook.notebook)
+tab5 = tk.Frame(Notebook.notebook)
+tab6 = tk.Frame(Notebook.notebook)
+tab7 = tk.Frame(Notebook.notebook)
+tab8 = tk.Frame(Notebook.notebook)
+tab9 = tk.Frame(Notebook.notebook)
+tab10 = tk.Frame(Notebook.notebook)
+tab11 = tk.Frame(Notebook.notebook)
+tab12 = tk.Frame(Notebook.notebook)
+tab13 = tk.Frame(Notebook.notebook)
 
-Tab = [tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, 
+TabFrame = [tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, 
        tab9, tab10, tab11, tab12, tab13]
 
+tabtype1 = Tabs()
+tabtype2 = Tabs()
+tabtype3 = Tabs()
+tabtype4 = Tabs()
+tabtype5 = Tabs()
+tabtype6 = Tabs()
+tabtype7 = Tabs()
+tabtype8 = Tabs()
+tabtype9 = Tabs()
+tabtype10 = Tabs()
+tabtype11 = Tabs()
+tabtype12 = Tabs()
+tabtype13 = Tabs()
+
+TabType = [tabtype1, tabtype2, tabtype3, tabtype4, tabtype5, tabtype6, 
+           tabtype7, tabtype8, tabtype9, tabtype10, tabtype11, tabtype12, tabtype13]
+
 ##############################################################
-
-""" GraphicFrame_Calib = tk.Frame(Notebook.CTFrame, borderwidth = 5, relief = 'ridge')
-GraphicFrame_Calib.grid(column = 0, row = 0, sticky = "nw", pady = 5)
-tk.Label(GraphicFrame_Calib, text = 'Calibration').grid(row = 0)
-
-GraphicFrame_Material = tk.Frame(Notebook.MTFrame, borderwidth = 5, relief = 'ridge')
-GraphicFrame_Material.grid(column = 0, row = 0, sticky = "nw", pady = 5)
-tk.Label(GraphicFrame_Material, text = 'Material').grid(row = 0) """
 
 window.run()
